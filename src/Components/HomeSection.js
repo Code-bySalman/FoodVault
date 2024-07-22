7459c92584131a2547f5e4a60af718b4c50a0c83
Code-bySalman <usmanis@student.iul.ac.in>
2024-07-22 15:35:23 +0530

    Deploy React app to GitHub Pages

--- src/Components/HomeSection.js
+++ src/Components/HomeSection.js
@@ -8,15 +8,15 @@ import { FaSearch } from "react-icons/fa";
 function HomeSection() {
   const navigate = useNavigate();
   const images =[
-    "/img/img_1.jpg",
-    "/img/img_2.jpg",
-    "/img/img_3.jpg",
-    "/img/img_4.jpg",
-    "/img/img_5.jpg",
-    "/img/img_6.jpg",
-    "/img/img_7.jpg",
-    "/img/img_8.jpg",
-    "/img/img_9.jpg",
+    "img/img_1.jpg",
+    "img/img_2.jpg",
+    "img/img_3.jpg",
+    "img/img_4.jpg",
+    "img/img_5.jpg",
+    "img/img_6.jpg",
+    "img/img_7.jpg",
+    "img/img_8.jpg",
+    "img/img_9.jpg",
   ]
   const [input, setInput] = useState("");
   const handleChange=(e)=>{
